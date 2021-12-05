18f7f7d3eb22c1ba97853164aeb3bbf9325fd60c
israel emoitologa <israelemoitologa@israels-MacBook-Pro-2.local>
2021-12-05 20:01:51 +0100

    set database  config

--- startup/db.js
+++ startup/db.js
@@ -4,8 +4,7 @@ const config = require('config');
 
 module.exports = function () {
     //connect to mongodb
-    const db = "mongodb://localhost/bart-ticketing-system";
-    //const db = config.get('MONGODB_URI');
+    const db = config.get('MONGODB_URI');
     mongoose.connect(db, { useNewUrlParser: true, useUnifiedTopology: true })
         .then(() =>
             winston.info(`Connected to ${db} ...`));
